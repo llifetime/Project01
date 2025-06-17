7dd8eb756aa38510fef9bdc577d85de298fc56af
Иван Бондарев <i.m.bondarev@yandex.ru>
2025-06-17 23:34:11 +0300

    Первый коммит

---
+++ src/masks.py
@@ -0,0 +1,19 @@
+def get_mask_card_number(card_numder: str) -> str:
+    """Маскирует 6 цифр и номера карты и
+
+    разбивает на 4 блока."""
+
+    masked_card_show = card_numder[:4] + " " + card_numder[4:6] + "**" + " **** " + card_numder[-4:]
+
+    return masked_card_show
+
+def get_mask_account(account_number:str) -> str:
+    """
+    маскировки номера банковского счета
+    :return: замаскированный счет
+    """
+    account_can_be_show = "**" + account_number[-4:]
+    return account_can_be_show
+
+
+print(get_mask_account("73654108430135874305"))
---
+++ src/widget.py
@@ -0,0 +1,40 @@
+def mask_card(card_info):
+    """
+    Маскирует номер карты, оставляя последние 4 цифры видимыми
+    Пример: Visa Platinum 7000792289606361 -> Visa Platinum **** **** **** 6361
+    """
+    # Разделяем тип и номер
+
+    parts = card_info.split()
+    if len(parts) != 2:
+        raise ValueError("Некорректный формат карты")
+
+    card_type = parts[0]
+    card_number = parts[1]
+
+    # Маскируем номер
+    masked_number = f"{card_type} {'*' * 12}{card_number[-4:]}"
+    return masked_number
+
+
+from datetime import datetime
+
+
+def get_date(date_string):
+    """
+    Преобразует дату из формата "2024-03-11T02:26:18.671407" в формат "11.03.2024"
+
+    Параметры:
+    date_string (str): строка с датой в формате ISO
+
+    Возвращает:
+    str: дата в формате "ДД.ММ.ГГГГ"
+    """
+    try:
+        # Разбираем входную строку в объект datetime
+        dt = datetime.fromisoformat(date_string)
+
+        # Форматируем в нужный формат
+        return dt.strftime("%d.%m.%Y")
+    except ValueError:
+        raise ValueError("Некорректный формат даты. Ожидался формат 'YYYY-MM-DDTHH:MM:SS.ffffff'")
